ac75da44c694782fb4c5bed4e69ffff2bac259b0
makazemafuu <102394629+makazemafuu@users.noreply.github.com>
2022-04-13 14:10:05 +0200

    Camera&Player
    Repositioned the camera (still have trouble with that aha), + added the PlayerMove script (need to add "jump").

--- CNAMazingHunt3D/Assets/Scripts/PlayerMove.cs
+++ CNAMazingHunt3D/Assets/Scripts/PlayerMove.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
+
+    public float speed = 5.0f;
+    private float motionUpDown;
+    private float motionLeftRight;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        //get player input
+        motionUpDown = Input.GetAxis("Horizontal");
+        motionLeftRight = Input.GetAxis("Vertical");
         
+        //move the player forward
+        transform.Translate(Vector3.forward * speed * Time.deltaTime * motionLeftRight);
+        transform.Translate(Vector3.right * Time.deltaTime * speed * motionUpDown);
+
     }
 }
