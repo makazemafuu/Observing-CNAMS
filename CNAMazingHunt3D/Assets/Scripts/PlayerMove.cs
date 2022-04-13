b2e1f34c0deb9927fb8be674e21456f0687a5cba
makazemafuu <102394629+makazemafuu@users.noreply.github.com>
2022-04-13 14:48:44 +0200

    Player
    Adding movement : jump (+ isOnGround).
    Make it so after 1 jump once back on the ground they can jump again (next commit).

--- CNAMazingHunt3D/Assets/Scripts/PlayerMove.cs
+++ CNAMazingHunt3D/Assets/Scripts/PlayerMove.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
 
     public float speed = 5.0f;
+    public float jumpForce = 5.0f;
+    public bool isOnGround = true;
     private float motionUpDown;
     private float motionLeftRight;
+    private Rigidbody playerRb;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        playerRb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -23,8 +27,15 @@ public class PlayerMove : MonoBehaviour
         motionLeftRight = Input.GetAxis("Vertical");
         
         //move the player forward
-        transform.Translate(Vector3.forward * speed * Time.deltaTime * motionLeftRight);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * motionLeftRight);
         transform.Translate(Vector3.right * Time.deltaTime * speed * motionUpDown);
+
+        //let the player jump
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        {
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
+        }
 
     }
 }
