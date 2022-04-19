2bf6baea04723b19253b637d1ef7bea1344e863f
makazemafuu <102394629+makazemafuu@users.noreply.github.com>
2022-04-19 14:35:05 +0200

    FlockBehavior Inspector
    Well, what I did was supposed to work but on a different version of Unity !
    But it's not, I can still see what I do with the Debug window rather than Inspector though.

--- CNAMazingHunt3D/Assets/Editor/CompositeBehaviorEditor.cs
+++ CNAMazingHunt3D/Assets/Editor/CompositeBehaviorEditor.cs
@@ -33,6 +33,7 @@ public class CompositeBehaviorEditor : Editor
             EditorGUI.LabelField(r, "Weights");
             r.y += EditorGUIUtility.singleLineHeight * 1.2f;
 
+            EditorGUI.BeginChangeCheck();
             for (int i = 0; i < cb.behaviors.Length; i++)
             {
                 r.x = 5f;
@@ -41,8 +42,74 @@ public class CompositeBehaviorEditor : Editor
                 r.x = 30f;
                 r.width = EditorGUIUtility.currentViewWidth - 95f;
                 cb.behaviors[i] = (FlockBehavior)EditorGUI.ObjectField(r, cb.behaviors[i], typeof(FlockBehavior), false);
+                r.x = EditorGUIUtility.currentViewWidth - 65f;
+                r.width = 60f;
+                cb.weights[i] = EditorGUI.FloatField(r, cb.weights[i]);
+                r.y += EditorGUIUtility.singleLineHeight * 1.1f;
             }
-
+            if (EditorGUI.EndChangeCheck())
+            {
+                EditorUtility.SetDirty(cb);
+            }
         }
+
+        EditorGUILayout.EndHorizontal();
+        r.x = 5f;
+        r.width = EditorGUIUtility.currentViewWidth - 10f;
+        r.y += EditorGUIUtility.singleLineHeight * 0.5f;
+
+        if (GUI.Button(r, "Add Behavior"))
+        {
+            // Add behavior
+            AddBehavior(cb);
+            EditorUtility.SetDirty(cb);
+        }
+
+        r.y += EditorGUIUtility.singleLineHeight * 1.5f;
+        if (cb.behaviors != null && cb.behaviors.Length > 0)
+        {
+            if (GUI.Button(r, "Remove Behavior"))
+            {
+                // Remove behavior
+                RemoveBehavior(cb);
+                EditorUtility.SetDirty(cb);
+            }
+        }
+    }
+
+    void AddBehavior(CompositeBehavior cb)
+    {
+        int oldCount = (cb.behaviors != null) ? cb.behaviors.Length : 0;
+        FlockBehavior[] newBehaviors = new FlockBehavior[oldCount + 1];
+        float[] newWeights = new float[oldCount + 1];
+        for (int i = 0; i < oldCount; i++)
+        {
+            newBehaviors[i] = cb.behaviors[i];
+            newWeights[i] = cb.weights[i];
+        }
+        newWeights[oldCount] = 1f;
+        cb.behaviors = newBehaviors;
+        cb.weights = newWeights;
+    }
+
+    void RemoveBehavior(CompositeBehavior cb)
+    {
+        int oldCount = cb.behaviors.Length;
+        if (oldCount == 1)
+        {
+            cb.behaviors = null;
+            cb.weights = null;
+            return;
+        }
+
+        FlockBehavior[] newBehaviors = new FlockBehavior[oldCount - 1];
+        float[] newWeights = new float[oldCount - 1];
+        for (int i = 0; i < oldCount - 1; i++)
+        {
+            newBehaviors[i] = cb.behaviors[i];
+            newWeights[i] = cb.weights[i];
+        }
+        cb.behaviors = newBehaviors;
+        cb.weights = newWeights;
     }
 }
